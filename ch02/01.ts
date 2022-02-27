02caebacabab2cbde168d50377640d36b538fd8a
kiyimba1 <edwineddie54@gmail.com>
2022-02-27 14:55:00 +0300

    nullish coalescing

--- ch02/01.ts
+++ ch02/01.ts
@@ -50,3 +50,12 @@ function checkDoorState(state: DoorState) {
             }
     }
 }
+
+// nullish coalescing
+function nullishCheck(a: number | undefined | null) {
+    console.log(`a : ${a ?? `undefined or null`}`)
+}
+
+nullishCheck(1)
+nullishCheck(null)
+nullishCheck(undefined)
